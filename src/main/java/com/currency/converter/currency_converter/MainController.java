854ce9b5d4f40d66cd4dc1a955a1fdbc20644f33
Jaras49 <jaras48@gmail.com>
2018-05-30 21:48:29 +0200

    mapping added to MainController test method

--- src/main/java/com/currency/converter/currency_converter/MainController.java
+++ src/main/java/com/currency/converter/currency_converter/MainController.java
@@ -2,11 +2,12 @@ package com.currency.converter.currency_converter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-    @RequestMapping()
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String test() {
 
         return "index.html";
