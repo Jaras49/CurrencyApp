4f5527e8cadb825c8f8c0bf33d23abfa3d69543e
Jaras49 <jaras48@gmail.com>
2018-07-24 15:52:01 +0200

    CryptoCompareResponseMapper added

---
+++ src/main/java/com/currency/converter/json/mapper/crypto/compare/CryptoCompareResponseMapper.java
@@ -0,0 +1,15 @@
+package com.currency.converter.json.mapper.crypto.compare;
+
+import com.currency.converter.json.mapper.JsonMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CryptoCompareResponseMapper implements JsonMapper {
+
+    @Override
+    public String processData(String json) {
+
+
+        return null;
+    }
+}
